3d47ac0a371f56d15ae8a0d215adddf807ae6d63
nameischethan <saichethanurumu@gmail.com>
2026-07-11 18:30:46 +0530

    Enable CORS

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,14 @@
 from routers.customers import router as customer_router
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],      # For development
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 from routers.suppliers import router as supplier_router
 from database import Base, engine
 from routers.items import router as item_router
@@ -7,15 +16,7 @@ from routers.purchase import router as purchase_router
 from routers.sales import router as sales_router
 from routers.dashboard import router as dashboard_router
 from routers.invoice import router as invoice_router
-from fastapi.middleware.cors import CORSMiddleware
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],   # For development/demo
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 # Import all models so SQLAlchemy creates the tables
 import models
